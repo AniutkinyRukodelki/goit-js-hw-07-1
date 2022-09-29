054de4fe682dbd03c39226f579bf8df9d41743ea
AniutkinyRukodelki <anyageiko91@gmail.com>
2022-09-29 19:46:51 +0300

    homework7

---
+++ js/01-gallery.js
@@ -0,0 +1,45 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+// console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const arrStrImade = [];
+ 
+function creatSrtElmGallImg(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`;
+    })
+    .join("");
+}
+
+gallery.insertAdjacentHTML("beforeend", creatSrtElmGallImg(galleryItems));
+
+gallery.addEventListener("click", openModalImage);
+function openModalImage(elv) {
+    elv.preventDefault();
+    if (elv.target.nodeName !== "IMG") {
+        return;
+    }
+    const instance = basicLightbox.create(
+        `<img src="${elv.target.dataset.source}" width="800" height="600">`
+    );
+    instance.show();
+
+    if (instance.visible()) {
+        gallery.addEventListener("keydown", onEsc);
+    }
+    function onEsc(evt) {
+        if (evt.key === "Escape") {
+            instance.close();
+            gallery.removeEventListener("keydown", onEsc);
+        }
+    }
+}
